398e993cc277c3877ed55dc2fdc1d0c9f80e1f47
josuemendezarmas <93894963+josuemendezarmas@users.noreply.github.com>
2025-02-04 00:22:37 -0800

    Updated immigration resources site

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 function toggleInfo() {
     let infoDiv = document.getElementById("info");
-    if (infoDiv.classList.contains("hidden")) {
-        infoDiv.classList.remove("hidden");
-    } else {
-        infoDiv.classList.add("hidden");
-    }
+    infoDiv.classList.toggle("hidden");
 }
